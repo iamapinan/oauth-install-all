8b91a175177cfced0f0e20a754d84b83068ff297
F. Kooman <fkooman@tuxed.net>
2012-11-02 15:40:03 +0100

    add some more data to saml_info page

--- res/saml_info.php
+++ res/saml_info.php
@@ -1,12 +1,22 @@
 <?php
-    require_once('{INSTALL_DIR}/ssp/lib/_autoload.php');
+    require_once('/Library/WebServer/Documents/oauth/ssp/lib/_autoload.php');
     $as = new SimpleSAML_Auth_Simple('default-sp');
     $as->requireAuth();
     $nameid = $as->getAuthData("saml:sp:NameID");
+    $idp = $as->getAuthData("saml:sp:IdP");
+    $sessionIndex = $as->getAuthData("saml:sp:SessionIndex");
     $attributes = $as->getAttributes();
     echo "<h1>NameID</h1>";
     echo "<pre>";
     print_r($nameid);
+    echo "</pre>";
+    echo "<h1>IdP</h1>";
+    echo "<pre>";
+    print_r($idp);
+    echo "</pre>";
+    echo "<h1>SessionIndex</h1>";
+    echo "<pre>";
+    print_r($sessionIndex);
     echo "</pre>";
     echo "<h1>Attributes</h1>";
     echo "<pre>";
