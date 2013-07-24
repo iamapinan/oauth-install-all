36bdbde87cc9c86e6f6706962347057884cfe20e
François Kooman <fkooman@tuxed.net>
2013-07-24 23:04:57 +0200

    work with new error handling

--- res/oauth/callback.php
+++ res/oauth/callback.php
@@ -2,6 +2,7 @@
 
 use fkooman\OAuth\Client\ClientConfig;
 use fkooman\OAuth\Client\Callback;
+use fkooman\OAuth\Client\AuthorizeException;
 use fkooman\OAuth\Client\SessionStorage;
 use Guzzle\Http\Client;
 
@@ -23,7 +24,12 @@ try {
 
     header("HTTP/1.1 302 Found");
     header("Location: {BASE_URL}/demo-oauth-app/index.php");
-
-} catch (\Exception $e) {
+} catch (AuthorizeException $e) {
+    // this exception is thrown by Callback when the OAuth server returns a 
+    // specific error message for the client, e.g.: the user did not authorize 
+    // the request
+    echo sprintf("ERROR: %s, DESCRIPTION: %s", $e->getMessage(), $e->getDescription());
+} catch (Exception $e) {
+    // other error, these should never occur in the normal flow
     echo sprintf("ERROR: %s", $e->getMessage());
 }
